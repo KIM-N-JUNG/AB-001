0af49c883d05f75589f5d90a2bca09129bd17af9
sunyoungJ <jsy7787@gmail.com>
2019-02-16 20:10:56 +0900

    Rearranged Assets Fixed a bug - unsynced time between Play and Pause Added score text on the Play

---
+++ Assets/game/Scripts/DodgeGame/Score.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private bool bPause;
+    private Text uiText;
+    public Timer timer;
+    public static int score;
+
+    public void Pause()
+    {
+        bPause = true;
+    }
+    public void Resume()
+    {
+        bPause = false;
+    }
+    public void Reset()
+    {
+        score = 0;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start Score");
+        uiText = GetComponent<Text>();
+
+        score = 0;
+        uiText.text = "0";
+        bPause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bPause == true)
+        {
+            return;
+        }
+
+        float time = timer.GetTime();
+        score = (int)(time * 150);
+
+        uiText.text = score.ToString();
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
