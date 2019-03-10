9649b8cde4daf8b8b66590b71779de4037408e18
sunyoungJ <jsy7787@gmail.com>
2019-03-10 21:22:14 +0900

    Removed background image on the Main Scene Added Constructor both scenes for initializing the GUI Added Font

---
+++ Assets/game/Scripts/DodgeGame/GameConstructor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameConstructor : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject shuttle;
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            ui.SetActive(true);
+            shuttle.SetActive(true);
+        }
+    }
+}
