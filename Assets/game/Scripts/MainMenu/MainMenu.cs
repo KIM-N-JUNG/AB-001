a9f6181ef330f2372454977304eb96f62cca9e10
sunyoungJ <jsy7787@gmail.com>
2019-05-15 23:28:48 +0900

    move login button to mainMenu

--- Assets/game/Scripts/MainMenu/MainMenu.cs
+++ Assets/game/Scripts/MainMenu/MainMenu.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    //GameObject toggleBackgroundSoundToggle;
-    public Toggle toggleBackgroundSoundToggle;
+    public GameObject mainMenuUI;
+    public GameObject privacyBoardUI;
+    public Toggle toggle_login;
 
     public void PlayGame()
     {
@@ -23,26 +24,86 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //toggleBackgroundSoundToggle = GameObject.Find("Toggle_backgroundSound");
-        //if (SingletonClass.Instance.bBGSound)
-        //{
-        //    toggleBackgroundSoundToggle.GetComponent<Toggle>().isOn = false;
-        //} else
-        //{
-        //    toggleBackgroundSoundToggle.GetComponent<Toggle>().isOn = true;
-        //}
-        //if (SingletonClass.Instance.bBGSound)
-        //{
-        //    toggleBackgroundSoundToggle.isOn = false;
-        //}
-        //else
-        //{
-        //    toggleBackgroundSoundToggle.isOn = true;
-        //}
+        toggle_login.isOn = SingletonClass.Instance.bLogin ? true : false;
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private bool IsAgreePrivacy()
+    {
+        bool isAgree = SingletonClass.Instance.bPrivacyAgreement && SingletonClass.Instance.bServiceAgreement;
+        Debug.Log("IsAgreePrivacy? : " + isAgree);
+        return isAgree;
+    }
+
+    private void ShowPrivacyBoard(bool toogleShow)
+    {
+        Debug.Log("ShowPrivacyBoard()");
+        mainMenuUI.SetActive(!toogleShow);
+        privacyBoardUI.SetActive(toogleShow);
+        //toggle_login.isOn = false;
+        Login(!toogleShow);
+    }
+
+    public void AgreePrivacy(bool agree)
+    {
+        Debug.Log("AgreePrivacy(" + agree + ")");
+        SingletonClass.Instance.bPrivacyAgreement = agree;
+        PlayerPrefs.SetInt("privacy", agree ? 1 : 0);
+        PlayerPrefs.Save();
+        if (IsAgreePrivacy())
+        {
+            ShowPrivacyBoard(false);
+        }
+    }
+
+    public void AgreeService(bool agree)
+    {
+        Debug.Log("AgreeService(" + agree + ")");
+        SingletonClass.Instance.bServiceAgreement = agree;
+        PlayerPrefs.SetInt("service", agree ? 1 : 0);
+        PlayerPrefs.Save();
+        if (IsAgreePrivacy())
+        {
+            ShowPrivacyBoard(false);
+        }
+    }
+
+    public void Login(bool bLogin)
+    {
+        if (SingletonClass.Instance.bLogin == bLogin)
+        {
+            return;
+        }
+
+        Debug.Log("bLogin - " + bLogin);
+
+        if (bLogin)
+        {
+            if (IsAgreePrivacy())
+            {
+                GPGSManager.GetInstance.LoginGPGS();
+                Debug.Log("LoginGPGS");
+            }
+            else
+            {
+                ShowPrivacyBoard(true);
+                return;
+            }
+        }
+        else
+        {
+            AgreePrivacy(false);
+            AgreeService(false);
+            GPGSManager.GetInstance.LogoutGPGS(false);
+            Debug.Log("LogoutGPGS");
+        }
+
+        SingletonClass.Instance.bLogin = bLogin;
+        PlayerPrefs.SetInt("login", bLogin ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
