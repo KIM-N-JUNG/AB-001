6c9514ac914d7b3d286583c6b78718880e4695b3
hercules <kjmhercules@gmail.com>
2019-07-14 21:20:36 +0900

    Admob 광고 추가 (option, RankBoard)

--- Assets/game/Scripts/Rankboard/RankboardConstructor.cs
+++ Assets/game/Scripts/Rankboard/RankboardConstructor.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
+using System;
+using Database.Service;
 
 public class RankboardConstructor : MonoBehaviour
 {
     public GameObject ui;
+
+    // for AdMob
+    private BannerView bannerView;
+    bool bAdsLoaded = false;
+
+    private const string APP_ID = "ca-app-pub-1339724987571025~1648266314";
+
+    private const string BANNER_ID = "ca-app-pub-1339724987571025/1266240866";
+
 
     // Start is called before the first frame update
     void Awake()
@@ -14,6 +26,8 @@ public class RankboardConstructor : MonoBehaviour
         if (SceneManager.GetActiveScene().buildIndex == (int)Constant.SceneNumber.RANK_BOARD)
         {
             ui.SetActive(true);
+
+            initBanner();
         }
     }
 
@@ -26,16 +40,57 @@ public class RankboardConstructor : MonoBehaviour
             {
                 if (Input.GetKey(KeyCode.Escape))
                 {
+                    Quit();
+
                     SceneManager.LoadScene((int)Constant.SceneNumber.MAIN_MENU);
                     return;
                 }
             }
         }
 #endif
-
     }
+
     void Start()
     {
         Debug.Log("RankboardConstructor Start");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+
+        if (bAdsLoaded)
+        {
+            bannerView.Hide();
+        }
+    }
+
+    private void initBanner()
+    {
+        Debug.Log("initBanner");
+        string adUnitId = string.Empty;
+
+#if UNITY_ANDROID
+        adUnitId = BANNER_ID;
+#elif UNITY_IOS
+        // adUnitId = ios_bannerAdUnitId;
+#endif
+        Debug.Log("initBanner adUnitId : " + adUnitId);
+
+        // bannerView = new BannerView(adUnitId, AdSize.MediumRectangle, AdPosition.Top);
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        bannerView.OnAdLoaded += HandleOnAdLoaded;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        Debug.Log("bannerView.LoadAd()");
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+        Debug.Log("HandleOnAdLoaded");
+        bAdsLoaded = true;
+        bannerView.Show();
     }
 }
